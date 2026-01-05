08074f4427ce68bda3eb47d5e0ca29d9ed6b8df9
Jailton Santos <jailtonmatos200@gmail.com>
2026-01-05 17:39:13 -0300

    style: define ANSI color constants for terminal UI styling

---
+++ anscii_sistem/collors.py
@@ -0,0 +1,14 @@
+
+
+# USAMOS O DICIONÁRIO PARA RETORNAR O CÓDIGO DE CORES PARA APLICAR AO TEXTO E/OU FUNDO.
+text_collor = {'white': '\033[30m', 'red': '\033[31m',
+               'green': '\033[32m', 'yellow': '\033[33m',
+               'blue': '\033[34m', 'purple': '\033[35m',
+               'light_blue': '\033[36m', 'gray': '\033[37m'}
+
+text_format = {'none': '\033[m', 'highlighted': '\033[1m', 'underline': '\033[4m', 'invert': '\033[7m'}
+
+background_collors = {'white': '\033[40m', 'red': '\033[41m',
+               'green': '\033[42m', 'yellow': '\033[43m',
+               'blue': '\033[44m', 'purple': '\033[45m',
+               'light_blue': '\033[46m', 'gray': '\033[47m'}
