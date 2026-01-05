f62255854ae6e7cc1f220a49272c9ebfe4a46df3
Jailton Santos <jailtonmatos200@gmail.com>
2026-01-05 17:41:50 -0300

    feat: add comprehensive dataset for gender identity selection

---
+++ extensiveList_generies/listMaxGender.py
@@ -0,0 +1,51 @@
+
+
+# LISTA COM MUITOS GÊNEROS SEXUAIS E SUBJETIVOS (MAIOR PARTE).
+gender_identitiesPunctuation = [
+    'transexual',
+    'bissexual',
+    'heterossexual',
+    'cis gênero',
+    'demissexual',
+    'bigênero',
+    'androgino',
+    'demigênero',
+    'neutrois',
+    'agênero',
+    'intergênero',
+    'terceiro gênero',
+    'não binário',
+    'pangênero',
+    'gênero ido',
+    'demiagênero',
+    'epiceno',
+    'transgênero',
+    'butch',
+    'aliagênero',
+    'poligênero'
+]
+
+# MESMA LISTA, PORÉM SEM ACENTOS OU PONTUAÇÕES.
+gender_identitiesNp = [
+    'transexual',
+    'bissexual',
+    'heterossexual',
+    'cis genero',
+    'demissexual',
+    'bigenero',
+    'androgino',
+    'demigenero',
+    'neutrois',
+    'agenero',
+    'intergenero',
+    'terceiro genero',
+    'nao binario',
+    'pangenro',
+    'genero ido',
+    'demiagenero',
+    'epiceno',
+    'transgenero',
+    'butch',
+    'aliagenero',
+    'poligenero'
+]
